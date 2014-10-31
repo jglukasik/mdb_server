e242fb645ce8345727a121261b98284fdbae4c2c
Joseph Lukasik <jglukasik@wisc.edu>
2014-10-30 23:24:58 -0400

    Return only min and max angles we saw the buildings at

--- query_ng.sql
+++ query_ng.sql
@@ -1,4 +1,6 @@
-CREATE OR REPLACE FUNCTION buildingSweep() RETURNS table(building_name text, heading integer) AS
+CREATE OR REPLACE 
+  FUNCTION buildingSweep(lng double precision, lat double precision) 
+  RETURNS table(building_name text, heading integer) AS
 $BODY$
 BEGIN
 FOR degrees IN 0..359 LOOP
@@ -11,8 +13,8 @@ FOR degrees IN 0..359 LOOP
       ST_Transform(
         ST_MakeLine(
           ARRAY[
-            ST_SetSRID(ST_MakePoint(-89.403465, 43.075343)::geometry, 4326),
-            ST_Project(ST_MakePoint(-89.403465, 43.075343), 500, radians(degrees))::geometry
+            ST_SetSRID(ST_MakePoint(lng, lat)::geometry, 4326),
+            ST_Project(ST_MakePoint(lng, lat), 500, radians(degrees))::geometry
           ]
         )
       , 900913)
@@ -22,7 +24,7 @@ FOR degrees IN 0..359 LOOP
     
     ORDER BY 
       ST_Distance(
-        ST_SetSRID(ST_MakePoint(-89.403465, 43.075343)::geometry, 900913),
+        ST_SetSRID(ST_MakePoint(lng, lat)::geometry, 900913),
         ST_SetSRID(ST_MakePoint(
           (ST_XMax(ST_Transform(way,4326))+ST_XMin(ST_Transform(way,4326)))/2,
           (ST_YMax(ST_Transform(way,4326))+ST_YMin(ST_Transform(way,4326)))/2
@@ -31,13 +33,17 @@ FOR degrees IN 0..359 LOOP
       )
 
       LIMIT 1;
-
 END LOOP;
-
 RETURN;
-
 END
 $BODY$
 LANGUAGE 'plpgsql';
 
-SELECT * FROM buildingSweep();
+SELECT building_name, MIN(heading), MAX(heading)
+FROM buildingSweep(-89.403465, 43.075343)
+GROUP BY building_name 
+ORDER BY max;
+
+
+
+
